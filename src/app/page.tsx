d8a2d45c8ed4b56cf1efcb5456714b88b558cce1
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-03-18 22:07:01 -0300

    feat(Home): Add GoogleMaps component and css adjustments to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Input } from "@/components/ui/input";
+import GoogleMaps from "@/components/GoogleMaps";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen">
-      <Input className="m-3 w-60" placeholder="Pesquisar cidade ou FATEC" />
+      <Input className="m-3 w-60 absolute z-10" placeholder="Pesquisar cidade ou FATEC" />
+      <GoogleMaps />
     </main>
   );
 }
