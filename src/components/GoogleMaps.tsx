4e4de84c50a5efcf74a72da75c0fefeffd3e17db
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-03-21 13:02:04 -0300

    feat(GoogleMaps): Make map attached to Sao Paulo state bounds

--- src/components/GoogleMaps.tsx
+++ src/components/GoogleMaps.tsx
@@ -1,10 +1,40 @@
 'use client'
 
-import { AdvancedMarker, APIProvider, Map } from "@vis.gl/react-google-maps";
+import {
+  AdvancedMarker,
+  APIProvider,
+  Map,
+  MapCameraChangedEvent
+} from "@vis.gl/react-google-maps"
 
 const SaoPauloStateCenterPosition: google.maps.LatLngLiteral = {
   lat: -22.43096057384716,
   lng: -49.130443886609
+}
+
+const SaoPauloStateBounds: google.maps.LatLngBoundsLiteral = {
+  north: -19.7796559,
+  south: -25.3579997,
+  east: -53.1101115,
+  west: -44.1613651
+}
+
+function isPositionWithinSaoPauloStateBounds(
+  position: google.maps.LatLngLiteral
+) {
+  const { lat, lng } = position
+  const { north, south, east, west } = SaoPauloStateBounds
+
+  return lat <= north && lat >= south && lng <= west && lng >= east
+}
+
+function handleCameraChange(event: MapCameraChangedEvent) {
+  const newPosition = event.detail.center
+
+  if (!isPositionWithinSaoPauloStateBounds(newPosition)) {
+    event.map.setZoom(7.5)
+    event.map.panTo(SaoPauloStateCenterPosition)
+  }
 }
 
 export default function GoogleMaps() {
@@ -21,6 +51,7 @@ export default function GoogleMaps() {
           minZoom={7.5}
           defaultZoom={7.5}
           defaultCenter={SaoPauloStateCenterPosition}
+          onCameraChanged={handleCameraChange}
           mapId={process.env.NEXT_PUBLIC_MAP_ID}
         >
           <AdvancedMarker position={santosFatecPosition} />
