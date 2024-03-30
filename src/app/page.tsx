c67834ffa67406f918acbe155f91e48ea7bb4223
As7Vezes <denysn60@gmail.com>
2024-03-30 16:13:58 -0300

    feat: add course list component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Input } from '@/components/ui/input'
 import GoogleMaps from '@/components/GoogleMaps'
+import { ScrollAreaDemo } from '@/components/CourseList'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen'>
       <Input
-        className='absolute z-10 m-3 w-60'
+        className='absolute z-10 m-3 w-72'
         placeholder='Pesquisar cidade ou FATEC'
       />
       <GoogleMaps />
+      <ScrollAreaDemo/>
     </main>
   )
 }
