fd2592e9e13c73027cf33fce1498337cc046b096
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-04-06 22:10:38 -0300

    Apply linting in Member component

--- src/components/Member/Member.tsx
+++ src/components/Member/Member.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
-import styles from './Member.module.css';
+import styles from './Member.module.css'
 
 import { Github, Linkedin } from 'lucide-react'
 
@@ -11,15 +11,24 @@ interface MemberProps {
   linkedinUrl: string
 }
 
-export default function Member({ photoUrl, name, githubUrl, linkedinUrl }: MemberProps) {
+export default function Member({
+  photoUrl,
+  name,
+  githubUrl,
+  linkedinUrl,
+}: MemberProps) {
   return (
     <div className='flex flex-col'>
-      <img src={photoUrl} className={styles.img}/>
+      <img src={photoUrl} className={styles.img} />
       <p className={styles.text}>{name}</p>
       <div className={styles.icons}>
-        <Link href={githubUrl}><Github /></Link>
-        <Link href={linkedinUrl}><Linkedin /></Link>
+        <Link href={githubUrl}>
+          <Github />
+        </Link>
+        <Link href={linkedinUrl}>
+          <Linkedin />
+        </Link>
       </div>
     </div>
-  );
-};
+  )
+}
