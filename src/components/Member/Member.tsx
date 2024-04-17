a2ffad67a101e6b70638f992eaf4cbd0090d3ed1
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-04-16 22:59:17 -0300

    feat(Member): Apply linting

--- src/components/Member/Member.tsx
+++ src/components/Member/Member.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
-
 import { Github, Linkedin } from 'lucide-react'
 
 interface MemberProps {
@@ -13,7 +12,7 @@ interface MemberProps {
 }
 
 export default function Member({
-  photoUrl,  
+  photoUrl,
   name,
   githubUrl,
   linkedinUrl,
@@ -21,22 +20,24 @@ export default function Member({
   return (
     <div className='flex flex-col items-center space-y-4 border border-solid border-purple-300 '>
       <div className='flex flex-col items-center space-y-4 border border-solid border-purple-300 '>
-      <Avatar className='relative'>
-        <AvatarImage className='border-2 border-solid border-black' src={photoUrl} alt={`${name} photo` } />
-        <AvatarFallback>{name}</AvatarFallback>
-      </Avatar>
-      <p className='font-bold'>{name}</p>
-      <div className="flex space-x-5" >
-        <Link href={githubUrl}>
-          <Github className="text-purple-600 h-6 w-6  ml-2 hover:text-purple-400 hover:animate-bounce"  />
-        </Link>
-        <Link href={linkedinUrl}>
-          <Linkedin className=" text-blue-600 h-6 w-6  mr-2 hover:text-blue-400 hover:animate-bounce"/>
-        </Link>
+        <Avatar className='relative'>
+          <AvatarImage
+            className='border-2 border-solid border-black'
+            src={photoUrl}
+            alt={`${name} photo`}
+          />
+          <AvatarFallback>{name}</AvatarFallback>
+        </Avatar>
+        <p className='font-bold'>{name}</p>
+        <div className='flex space-x-5'>
+          <Link href={githubUrl}>
+            <Github className='ml-2 h-6 w-6  text-purple-600 hover:animate-bounce hover:text-purple-400' />
+          </Link>
+          <Link href={linkedinUrl}>
+            <Linkedin className=' mr-2 h-6 w-6  text-blue-600 hover:animate-bounce hover:text-blue-400' />
+          </Link>
         </div>
       </div>
     </div>
   )
 }
-
-
