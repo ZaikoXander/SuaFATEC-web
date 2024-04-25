aca6390122856e8e1a538b4adddb1759416f08ad
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-04-24 21:46:15 -0300

    feat(CourseInfoAtoms): Create CourseInfo opened state atoms

--- src/atoms/sheets.ts
+++ src/atoms/sheets.ts
@@ -6,4 +6,15 @@ const openCourseCommentsAtom = atom(null, (_get, set) =>
   set(courseCommentsOpenAtom, true),
 )
 
-export { courseCommentsOpenAtom, openCourseCommentsAtom }
+const courseInfoOpenAtom = atom(false)
+
+const openCourseInfoAtom = atom(null, (_get, set) =>
+  set(courseInfoOpenAtom, true),
+)
+
+export {
+  courseCommentsOpenAtom,
+  openCourseCommentsAtom,
+  courseInfoOpenAtom,
+  openCourseInfoAtom,
+}
