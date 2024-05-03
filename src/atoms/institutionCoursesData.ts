7512e1fb48a51679e61329a8372b5de209843209
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-05-03 12:51:59 -0300

    Add duration to coursesAtom Course

--- src/atoms/institutionCoursesData.ts
+++ src/atoms/institutionCoursesData.ts
@@ -3,6 +3,7 @@ import { atom } from 'jotai'
 interface Course {
   id: number
   title: string
+  duration: number
 }
 
 const coursesAtom = atom<Course[]>([
@@ -10,50 +11,62 @@ const coursesAtom = atom<Course[]>([
     id: 1,
     title:
       'Design de Produto com ênfase em Processos de Produção e Industrialização',
+    duration: 6,
   },
   {
     id: 2,
     title: 'Desenvolvimento Web Avançado',
+    duration: 6,
   },
   {
     id: 3,
     title: 'Machine Learning Fundamentals',
+    duration: 6,
   },
   {
     id: 4,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
   {
     id: 5,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
   {
     id: 6,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
   {
     id: 7,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
   {
     id: 8,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
   {
     id: 9,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
   {
     id: 10,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
   {
     id: 11,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
   {
     id: 12,
     title: 'Gestão de Projetos Ágeis',
+    duration: 6,
   },
 ])
 
