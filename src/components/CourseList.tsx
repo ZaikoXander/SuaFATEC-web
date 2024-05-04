a78b120b1f0a69c48907fb9b86adb53cf2f646c5
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-05-04 13:29:09 -0300

    feat(CourseList): Replace courses atom by institutionCourses atom and properly sets the list buttons to open the right course

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -3,7 +3,12 @@
 import { Fragment } from 'react'
 
 import { useAtomValue, useSetAtom } from 'jotai'
-import { coursesAtom, selectedCourseAtom } from '@/atoms/institutionCoursesData'
+import {
+  type Course,
+  institutionCoursesAtom,
+  selectedCourseAtom,
+} from '@/atoms/institutionCoursesData'
+import { openCourseInfoAtom } from '@/atoms/sheets'
 
 import { Button } from './ui/button'
 import { SheetClose } from './ui/sheet'
@@ -11,25 +16,31 @@ import { ScrollArea } from './ui/scroll-area'
 import { Separator } from './ui/separator'
 
 export default function CourseList() {
-  const courses = useAtomValue(coursesAtom)
+  const institutionCourses = useAtomValue(institutionCoursesAtom)
   const setSelectedCourse = useSetAtom(selectedCourseAtom)
+  const openCourseInfo = useSetAtom(openCourseInfoAtom)
+
+  function handleSelectCourse(course: Course) {
+    setSelectedCourse(course)
+    openCourseInfo()
+  }
 
   return (
     <ScrollArea className='h-80 w-full rounded-md border p-4'>
       <h4 className='mb-4 text-lg font-bold leading-none'>
         Cursos disponíveis
       </h4>
-      {courses.map((course) => {
-        const lastCourseId = courses.at(-1)?.id
+      {institutionCourses.map((course) => {
+        const lastCourseId = institutionCourses.at(-1)?.id
         if (lastCourseId === course.id) {
           return (
             <SheetClose key={course.id} asChild>
               <Button
                 variant='outline'
                 className='h-auto w-full text-pretty'
-                onClick={() => setSelectedCourse(course)}
+                onClick={() => handleSelectCourse(course)}
               >
-                {course.title}
+                {course.name}
               </Button>
             </SheetClose>
           )
@@ -37,13 +48,13 @@ export default function CourseList() {
 
         return (
           <Fragment key={course.id}>
-            <SheetClose key={course.id} asChild>
+            <SheetClose asChild>
               <Button
                 variant='outline'
                 className='h-auto w-full text-pretty'
-                onClick={() => setSelectedCourse(course)}
+                onClick={() => handleSelectCourse(course)}
               >
-                {course.title}
+                {course.name}
               </Button>
             </SheetClose>
             <Separator className='my-2' />
