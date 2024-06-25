aeaca90bde7b2d1e9a54aa92fb4a49b9dd89fdf8
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-06-24 23:06:50 -0300

    feat(LoginForm): Improve using shadcn Form, allow enter on fields to submit

--- src/components/admin/LoginForm.tsx
+++ src/components/admin/LoginForm.tsx
@@ -1,25 +1,30 @@
 'use client'
 
-import { ChangeEvent, useState } from 'react'
-
 import { useRouter } from 'next/navigation'
 
-import { atom, useAtom, useSetAtom } from 'jotai'
+import { useState } from 'react'
+
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { useSetAtom } from 'jotai'
 import { adminAtom } from '@/atoms/admin'
 
 import api from '@/lib/api'
 import { AxiosError } from 'axios'
 
 import { Button } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form'
 
 interface AdminsAuthResponse {
   admin: {
@@ -29,23 +34,29 @@ interface AdminsAuthResponse {
   token: string
 }
 
-const credentialsAtom = atom({
-  name: '',
-  password: '',
+const formSchema = z.object({
+  name: z
+    .string({ required_error: 'O nome é obrigatório' })
+    .min(1, 'O nome deve conter pelo menos 1 caractere'),
+  password: z
+    .string({ required_error: 'A senha é obrigatória' })
+    .min(1, 'A senha deve conter pelo menos 1 caractere'),
 })
 
 export function LoginForm() {
-  const [credentials, setCredentials] = useAtom(credentialsAtom)
   const setAdmin = useSetAtom(adminAtom)
   const router = useRouter()
   const [wrongCredentials, setWrongCredentials] = useState(false)
 
-  function handleCredentialsChange(event: ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target
-    setCredentials((prev) => ({ ...prev, [name]: value }))
-  }
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      password: '',
+    },
+  })
 
-  async function handleSignIn() {
+  async function onSubmit(credentials: z.infer<typeof formSchema>) {
     try {
       const {
         data: { admin, token },
@@ -71,37 +82,44 @@ export function LoginForm() {
       <CardHeader>
         <CardTitle className='text-2xl'>Login</CardTitle>
       </CardHeader>
-      <CardContent className='grid gap-4'>
-        <div className='grid gap-2'>
-          <Label htmlFor='name'>Nome</Label>
-          <Input
-            id='name'
-            name='name'
-            type='text'
-            placeholder='Gabriel'
-            required
-            onChange={handleCredentialsChange}
-          />
-        </div>
-        <div className='grid gap-2'>
-          <Label htmlFor='password'>Senha</Label>
-          <Input
-            id='password'
-            name='password'
-            type='password'
-            required
-            onChange={handleCredentialsChange}
-          />
-        </div>
-        {wrongCredentials && (
-          <p className='text-sm text-red-500'>Nome ou senha incorretos.</p>
-        )}
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Gabriel Fernandes' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input type='password' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {wrongCredentials && (
+              <p className='text-sm text-red-500'>Nome ou senha incorretos.</p>
+            )}
+            <Button type='submit' className='w-full'>
+              Entrar
+            </Button>
+          </form>
+        </Form>
       </CardContent>
-      <CardFooter>
-        <Button className='w-full' onClick={handleSignIn}>
-          Entrar
-        </Button>
-      </CardFooter>
     </Card>
   )
 }
